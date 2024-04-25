f3e74a262f211f71ad6a0f395dae3f739ffb4b8f
Hanuš Valenta <93769207+hanusvalenta@users.noreply.github.com>
2024-04-25 21:41:58 +0200

    small changes in script

--- Scripts/Dio.cs
+++ Scripts/Dio.cs
@@ -7,13 +7,11 @@ public class Dio : Node
 	// private int a = 2;
 	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("ZaWarudo ready...");
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
